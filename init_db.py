aafec1ac2280672d6810be4085a26b402b84d0ab
Aysayris <umedtutishoev@gmail.com>
2023-11-04 23:32:02 +0300

    include_photos

--- init_db.py
+++ init_db.py
@@ -1,20 +1,21 @@
 import sqlite3
-
+# Создаем соединение с базой данных и выполняем SQL-скрипт
 connection = sqlite3.connect('database.db')
-
-
 with open('schema.sql') as f:
     connection.executescript(f.read())
 
+# Создаем курсор для выполнения SQL-запросов
 cur = connection.cursor()
 
-cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
-            ('First Post', 'Content for the first post')
+# Вставляем данные в таблицу "posts"
+cur.execute("INSERT INTO posts (title, content, photo_filename) VALUES (?, ?, ?)",
+            ('First Post', 'Content for the first post','photo for the first post')
             )
 
-cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
-            ('Second Post', 'Content for the second post')
+cur.execute("INSERT INTO posts (title, content, photo_filename) VALUES (?, ?, ?)",
+            ('Second Post', 'Content for the second post','photo for the second post')
             )
 
+# Фиксируем изменения в базе данных и закрываем соединение
 connection.commit()
 connection.close()
